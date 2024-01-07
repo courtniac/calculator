77bc95a782c44f5591f9e3c9aebee18d47cbc0ad
Courtnie Chan <20526504+courtniac@users.noreply.github.com>
2024-01-06 21:06:50 -0800

    add listeners to the buttons and handle value change

---
+++ script.js
@@ -0,0 +1,58 @@
+const buttons = document.querySelectorAll(".btn");
+const display = document.getElementsByClassName("displayValue")[0];
+let firstVal;
+let secondVal;
+let operator;
+let displayVal = "0";
+
+for (let button of buttons) {
+    button.addEventListener("click", (e) => handleClick(e));
+}
+
+function handleClick(e) {
+    e.preventDefault();
+
+    switch (e.target.id) {
+        case "clear":
+            console.log("clear");
+            break;
+        case "negation":
+            console.log("neg");
+            break;
+        case "percent":
+            console.log("percent");
+            break;
+        case "divide":
+        case "multiply":
+        case "subtract":
+        case "add":
+        case "equals":
+            console.log("operation");
+            break;
+        default:
+            handleValueChange(e.target.innerText);
+            break;
+    }
+}
+
+function handleValueChange(value) {
+    if (displayVal === "0" && value === "0") {
+        return;
+    }
+
+    if (value === "." && displayVal.indexOf(value) !== -1) {
+        return;
+    }
+    
+    if (displayVal === "0") {
+        displayVal = value;
+    } else {
+        displayVal += value;
+    }
+
+    updateDisplayValue();
+}
+
+function updateDisplayValue() {
+    display.innerText = displayVal;
+}
